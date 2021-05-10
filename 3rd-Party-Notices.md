8af6f709e121422b352d0d0d7974d9284022f348
Matt Seemon <contact@mattseemon.in>
2021-05-10 13:45:24 +0530

    Solution, Base Projects and more
    1. Created Solution & Base Projects
    	- Vault-Core - Vault Core Library
    	- Vault-UX - Vault User Experience
    2. Implemented Versioning Strategy
    3. Included Project Assets (Icons, PNG, etc)
    4. Updated README.md
    5. Created 3rd-Party-Notices.md

---
+++ 3rd-Party-Notices.md
@@ -0,0 +1,8 @@
+# 3rd-Party Software for [Vault](https://github.com/mattseemon/Vault.git)
+
+The following 3rd-party software packages are distributed with **Vault**. 
+
+## Direct Dependencies
+
+Package|Licenses
+-------|--------
---
+++ src/Versioning/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿/*
+ Major: 0   
+ Minor: 1   
+ Patch: 1   
+ Build: 36691   
+Suffix: alpha 
+
+This file is generated automatically with each build. 
+Any changes made manually will be lost with the next build.
+*/
+
+using System.Reflection;
+using System.Windows;
+
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1.1.36691")]
+[assembly: AssemblyInformationalVersion("0.1.1-alpha")]
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
